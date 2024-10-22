b9610de57ec2e4a6ef6eebc3f0f3a26209cac205
[Aman Sinha] <1998amansinha@gmail.com>
2024-10-22 09:46:56 +0530

    Validation Added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,10 @@
 import { useRef, useState } from "react";
 import Header from "./Header";
+import { validateData } from "../utils/validate";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const toggleForm = () => {
     setIsSignInForm(!isSignInForm);
@@ -12,8 +14,11 @@ const Login = () => {
   const passwordRef = useRef(null);
 
   const handleButtonClick = () => {
-    console.log(emailRef.current.value);
-    console.log(passwordRef.current.value);
+    const validationMessage = validateData(
+      emailRef.current.value,
+      passwordRef.current.value
+    );
+    setErrorMessage(validationMessage);
   };
 
   return (
@@ -25,55 +30,57 @@ const Login = () => {
           alt="BackGround"
         />
       </div>
-      <div className="absolute bg-gray-950 opacity-80 w-1/3 h-2/3 my-20 mx-auto right-0 left-0 rounded-lg">
-        <form className="mx-8 bg-opacity-80" onSubmit={(e) => e.preventDefault()}>
-          <h2 className="text-3xl font-bold text-white mx-10 mt-12 mb-6">
-            {isSignInForm ? "Sign In" : "Sign up"}
-          </h2>
-          {!isSignInForm && (
-            <input
-              className="w-3/4 p-3 my-3 mx-10 rounded-sm bg-gray-900 text-white outline outline-offset-2 outline-1 outline-white"
-              type="text"
-              placeholder="Full Name"
-            />
-          )}
+      <form
+        className="bg-opacity-80 absolute bg-gray-950 w-1/3 h-2/3 my-20 mx-auto right-0 left-0 rounded-lg"
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <h2 className="text-3xl font-bold text-white mx-10 mt-12 mb-6">
+          {isSignInForm ? "Sign In" : "Sign up"}
+        </h2>
+        {!isSignInForm && (
           <input
             className="w-3/4 p-3 my-3 mx-10 rounded-sm bg-gray-900 text-white outline outline-offset-2 outline-1 outline-white"
             type="text"
-            placeholder="Email or Phone number"
-            ref={emailRef}
+            placeholder="Full Name"
           />
-          <input
-            className="w-3/4 p-3 my-3 mx-10 rounded-sm bg-gray-900 outline outline-offset-2 outline-1 outline-white text-white"
-            type="password"
-            placeholder="Password"
-            ref={passwordRef}
-          />
-          <button
-            onClick={handleButtonClick}
-            className="w-3/4 p-3 my-5 mx-10 rounded-md text-white font-semibold bg-red-600 text-lg hover:bg-red-800"
-          >
-            {isSignInForm ? "Sign In" : "Sign up"}
-          </button>
-          <p className="text-white mx-10 mt-10" onClick={toggleForm}>
-            {isSignInForm ? (
-              <>
-                New to Netflix?{" "}
-                <span className="cursor-pointer hover:underline">
-                  Sign up now
-                </span>
-              </>
-            ) : (
-              <>
-                Already a User?{" "}
-                <span className="cursor-pointer hover:underline">
-                  Sign in now
-                </span>
-              </>
-            )}
-          </p>
-        </form>
-      </div>
+        )}
+        <input
+          className="w-3/4 p-3 my-3 mx-10 rounded-sm bg-gray-900 text-white outline outline-offset-2 outline-1 outline-white"
+          type="text"
+          placeholder="Email or Phone number"
+          ref={emailRef}
+        />
+        <input
+          className="w-3/4 p-3 my-3 mx-10 rounded-sm bg-gray-900 outline outline-offset-2 outline-1 outline-white text-white"
+          type="password"
+          placeholder="Password"
+          ref={passwordRef}
+        />
+        <p className="text-red-600 mx-10 font-bold text-lg">{errorMessage}</p>
+        <button
+          onClick={handleButtonClick}
+          className="w-3/4 p-3 my-5 mx-10 rounded-md text-white font-semibold bg-red-600 text-lg hover:bg-red-800"
+        >
+          {isSignInForm ? "Sign In" : "Sign up"}
+        </button>
+        <p className="text-white mx-10 mt-10" onClick={toggleForm}>
+          {isSignInForm ? (
+            <>
+              New to Netflix?{" "}
+              <span className="cursor-pointer hover:underline">
+                Sign up now
+              </span>
+            </>
+          ) : (
+            <>
+              Already a User?{" "}
+              <span className="cursor-pointer hover:underline">
+                Sign in now
+              </span>
+            </>
+          )}
+        </p>
+      </form>
     </div>
   );
 };
