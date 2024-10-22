f99f30c809b9e5077ecabbded9f67262490718be
[Aman Sinha] <1998amansinha@gmail.com>
2024-10-22 09:47:43 +0530

    Components created and import are done

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,7 +1,7 @@
-export const validate = (email, password) => {
-  const isEmailValidate = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+export const validateData = (email, password) => {
+  const isEmailValidate = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
   const isPasswordValidate =
-    /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"/.test(
+    /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/.test(
       password
     );
 
